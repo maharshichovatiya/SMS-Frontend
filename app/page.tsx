d898795061984c065f5c121743dda558fe358a43
harshal246 <bhattharshal71@gmail.com>
2026-02-19 10:13:37 +0530

    feat: setup josefin sans font

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  function HandleSubmit(): string {
+    return "string";
+  }
+
+  HandleSubmit();
+
+  return <div className="text-2xl">School Management System</div>;
+}
