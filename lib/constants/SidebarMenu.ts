9a8a63468a5a76958130ac5c668153c73f0da50a
Mahamad ali Kadiwal <mohdkadiwal786@gmail.com>
2026-03-17 10:53:36 +0530

    feat: add icon in the sidebar

--- lib/constants/SidebarMenu.ts
+++ lib/constants/SidebarMenu.ts
@@ -131,17 +131,17 @@ export const SIDEBAR_MENUS: RoleMenus = {
           path: "/subjects",
         },
         {
-          icon: "BookOpen",
+          icon: "FileText",
           label: "Resources",
           path: "/resources",
         },
         {
-          icon: "BookOpen",
+          icon: "Bell",
           label: "Notifications",
           path: "/notifications",
         },
         {
-          icon: "BookOpen",
+          icon: "Calendar",
           label: "Notice-board",
           path: "/notice-board",
         },
